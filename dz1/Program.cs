bf067f5d3480ed271e0cb22d8242168c8b71e5c1
Kutman <nurkutman1997@gmail.com>
2023-06-11 23:56:23 +0600

    first home task done

---
+++ dz1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу которая на вход принимает два числа и выдает наименьшее и наибольшее
+
+Console.Write ("Введите первое число ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+
+Console.Write ("Введите второе число ");
+number = Console.ReadLine()!;
+int num2 = int.Parse(number);
+if (num > num2)
+        {
+            Console.WriteLine($"{num} = Max, {num2} = Min");
+        }
+        else if (num2 > num)
+        {
+            Console.WriteLine( $"{num2} = Max, {num} = Min");
+        }
+        else
+        {
+            Console.WriteLine("Числа равны");
+        }
+
+        Console.ReadLine();
