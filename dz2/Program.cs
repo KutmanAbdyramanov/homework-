74f2712481063e02dd53b2072fb406db03325434
Kutman <nurkutman1997@gmail.com>
2023-06-12 00:40:56 +0600

    2nd homeTask done

---
+++ dz2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать код который выдаст большее число из заданных трех
+
+Console.Write ("Введите первое число ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+
+Console.Write ("Введите второе число ");
+number = Console.ReadLine()!;
+int num2 = int.Parse(number);
+
+Console.Write ("Введите третье число ");
+number = Console.ReadLine()!;
+int num3 = int.Parse(number);
+if (num > num2 && num > num3)
+{
+Console.WriteLine("Наибольшее число: " + num);
+}
+else if (num2 > num && num2 > num3)
+{
+Console.WriteLine("Наибольшее число: " + num2);
+}
+else if (num3 > num && num3 > num2)
+{
+Console.WriteLine("Наибольшее число: " + num3);
+}
+else
+{
+Console.WriteLine("Числа равны");
+}
+
+
+
+
+
