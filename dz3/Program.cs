f015f5f20707aa37cb7f128894960567f4641f7c
Kutman <nurkutman1997@gmail.com>
2023-06-12 00:52:32 +0600

    3rd hw done

---
+++ dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите код проверяющий является ли заданное число четным
+
+Console.Write ("Введите число ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+if (num % 2 == 0)
+{
+Console.WriteLine("Число является четным");
+}
+else
+{
+Console.WriteLine("Число не является четным");
+}
