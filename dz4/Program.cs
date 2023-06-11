01411e5eb048db411e002e417412c752dd4a5bc3
Kutman <nurkutman1997@gmail.com>
2023-06-12 01:21:20 +0600

    HomeTaskDone

---
+++ dz4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.*/
+
+ Console.Write ("Введите число ");
+string number = Console.ReadLine( )!;
+int num = Convert.ToInt32(number);
+int start = 0;
+while (start <= num)
+{
+    if (num % 2 == 0)
+{
+Console.Write(start+" , ");
+start += 2;
+
+}
+}
